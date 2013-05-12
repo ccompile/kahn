4c2bd2f2ad6838ed11c4d20394a7754489752469
Antonin Delpeuch <antonin@delpeuch.eu>
2013-05-12 16:32:36 +0200

    Simplified doco

--- seq.ml
+++ seq.ml
@@ -1,21 +1,26 @@
 
+(* Un processus est soit une valeur (c'est à dire un return tout prêt)
+ * soit une fonction, qui, quand on l'exécute, renvoie un nouveau processus
+ * du même type *)
 type 'a process =
-    | Proc of (unit -> 'a process)
     | Val of 'a
+    | Proc of (unit -> 'a process)
 
+(* Les canaux sont juste des files classiques *)
 type 'a channel = 'a Queue.t
 type 'a in_port = 'a channel
 type 'a out_port = 'a channel
-
-let _fresh_chanid = ref 0
 
 let new_channel () =
     let chan = Queue.create () in
     (chan, chan)
 
+(* Il ne faut pas exécuter tout de suite le put,
+ * donc on ajoute Proc (fun () -> …) *)
 let put v c =
     Proc (fun () -> Val (Queue.push v c))
     
+(* Idem *)
 let rec get c =
     Proc (fun () ->
         try
@@ -23,15 +28,23 @@ let rec get c =
         with Queue.Empty ->
             get c)
 
+(* Stratégie retenue pour le doco :
+ * On parcourt la liste des processus.
+ * Si aucun d'entre eux n'est de la forme Proc qqch,
+ * c'est que tous ont été évalués, donc on termine.
+ * Sinon, si un des processus était un Proc,
+ * alors on le lance pendant une étape, on le met
+ * à la fin de la liste et on recommence.
+ *)
 let doco l =
-    let rec step accu = function
-        | [] -> (false,accu)
-        | (Val v)::t -> step ((Val v)::accu) t
-        | (Proc u)::t -> (true, (t @ ((u ())::accu)))
+    let rec step = function
+        | [] -> None
+        | (Val v)::t -> step t
+        | (Proc u)::t -> Some (t @ [u ()])
     in
-    let rec run_list lst = match step [] lst with
-       | (false,_) -> Val ()
-       | (true,l) -> Proc (fun () -> run_list l)
+    let rec run_list lst = match step lst with
+       | None -> Val ()
+       | Some l -> Proc (fun () -> run_list l)
     in
     Proc (fun () -> run_list l)
         
