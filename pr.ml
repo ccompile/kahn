44b66892fe7fd884fcb20f4ad152058a5bc924ea
Antonin Delpeuch <antonin@delpeuch.eu>
2013-05-26 22:51:47 +0200

    Simplified pr.ml, speed has doubled

--- pr.ml
+++ pr.ml
@@ -1,7 +1,7 @@
 type 'a in_port = Unix.file_descr
 type 'a out_port = Unix.file_descr
 
-type 'a process = ('a out_port -> unit)
+type 'a process = (unit -> 'a)
 
 let flags = [Marshal.Closures]
 
@@ -22,49 +22,26 @@ let mrecv (c : 'a in_port) =
    (* pdeg "Mashal.from_channel"; *)
     ((Marshal.from_channel (Unix.in_channel_of_descr c)) : 'a)
 
-let return v out =
-    msend out v;
-    Unix.close out
+let return v () = v
 
-let put v c out =
-    msend c v;
-    return () out
+let put v c () =
+    msend c v
 
-let rec get (c : 'a in_port) out =
-    let v = mrecv c in
-    return v out
+let rec get (c : 'a in_port) () =
+    mrecv c
  
-let start (p : 'a process) =
-    let (r,w) = new_channel () in
+let start (p : unit process) =
     let v = Unix.fork() in
     (match v with
-     | 0 -> Unix.close w;
-            (v,r)
-     | pid -> Unix.close r;
-              p w;
-              exit 0)
+     | 0 -> p (); exit 0
+     | pid -> pid)
 
-let doco l out =
+let doco l () =
     let ths = List.map start l in
-    List.iter (fun (pid,_) -> let _ = Unix.waitpid [] pid in ()) ths;
-    return () out
+    List.iter (fun pid -> let _ = Unix.waitpid [] pid in ()) ths
 
-let bind (e : 'a process) e' out =
-    let (r,w) = new_channel () in
-    e w;
-    let v = (mrecv r : 'a) in
-    Unix.close r;
-    e' v out
-
+let bind (e : 'a process) e' () =
+    e' (e ()) ()
 
 let run (e : 'a process) =
-    let (pid,output) = start e in
-    (match Unix.waitpid [] pid with
-    | (_,Unix.WEXITED(0)) ->
-            let v = mrecv output in
-            Unix.close output;
-            v
-    | _ -> Unix.close output;
-           raise RuntimeError)
-
-
+    e ()
